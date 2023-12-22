cf1189b8a4e2502fae7bcebe043c2af7b2a1e51f
meddizzle316 <meddizzle316@gmail.com>
2023-12-22 13:56:10 -0600

    betty fixes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_is_leaf - checks to see if node is a leaf
  * @node: given node to check
- * 
+ *
  * Return: returns 1 if node is leaf otherwise returns 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
