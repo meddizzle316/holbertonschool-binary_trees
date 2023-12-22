7a80f1b63de09dd50d7449c2793ee8bf1a27c6ce
meddizzle316 <meddizzle316@gmail.com>
2023-12-22 15:12:08 -0600

    weird null check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,6 +9,8 @@ size_t binary_tree_height_recursion(const binary_tree_t *tree);
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	return ((binary_tree_height_recursion(tree)) - 1);
 }
 
