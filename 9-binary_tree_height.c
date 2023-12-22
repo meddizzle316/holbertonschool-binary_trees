b7ae4b8f043bfd02ba6109d6c703f9e983cf171c
meddizzle316 <meddizzle316@gmail.com>
2023-12-22 15:10:04 -0600

    managed to tweak measurements a bit to fit format

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+size_t binary_tree_height_recursion(const binary_tree_t *tree);
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to be measured
+ *
+ * Return: the height or 0 if fail
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return ((binary_tree_height_recursion(tree)) - 1);
+}
+
+/**
+ * binary_tree_height_recursion - measures the height of a binary tree
+ * @tree: root node of the tree to measure
+ *
+ * Return: the height or 0 if fail
+ */
+size_t binary_tree_height_recursion(const binary_tree_t *tree)
+{
+	int lheight = 0, rheight = 0;
+
+	if (tree == NULL)
+		return (0);
+	lheight = binary_tree_height_recursion(tree->left);
+	rheight = binary_tree_height_recursion(tree->right);
+	if (lheight > rheight)
+		return (lheight + 1);
+	else
+		return (rheight + 1);
+}
