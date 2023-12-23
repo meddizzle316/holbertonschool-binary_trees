a289553f1971dbc9ce8142efcf053659e7083476
meddizzle316 <meddizzle316@gmail.com>
2023-12-22 19:41:45 -0600

    first push of 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,18 +3,18 @@
 
 /**
  * binary_tree_height_recursion - measures the height of a binary tree
- * @tree: root node of the tree to measure
+ * @t: root node of the tree to measure
  *
  * Return: the height or 0 if fail
  */
-size_t binary_tree_height_recursion(const binary_tree_t *tree)
+size_t binary_tree_height_recursion(const binary_tree_t *t)
 {
 	int lheight = 0, rheight = 0;
 
-	if (tree == NULL)
+	if (t == NULL)
 		return (0);
-	lheight = binary_tree_height_recursion(tree->left);
-	rheight = binary_tree_height_recursion(tree->right);
+	lheight = binary_tree_height_recursion(t->left);
+	rheight = binary_tree_height_recursion(t->right);
 	if (lheight > rheight)
 		return (lheight + 1);
 	else
@@ -23,16 +23,17 @@ size_t binary_tree_height_recursion(const binary_tree_t *tree)
 
 /**
  * binary_tree_is_full - checks if a binary tree is full
- * @tree: root node to be checked
+ * @t: root node to be checked
  * Return: 1 if full or 0 if failed or not full
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *t)
 {
-	int lheight, rheight;
+	int lh, rh;
 
-	if (tree == NULL)
+	if (t == NULL)
 		return (1);
-	lheight = binary_tree_height_recursion(tree->left);
-	rheight = binary_tree_height_recursion(tree->right);
-	return (lheight == rheight && binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	lh = binary_tree_height_recursion(t->left);
+	rh = binary_tree_height_recursion(t->right);
+	return (lh == rh && binary_tree_is_full(t->left)
+		&& binary_tree_is_full(t->right));
 }
