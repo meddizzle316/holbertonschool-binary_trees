170c4a77c62866bc981c5b09862c79d7e756b881
meddizzle316 <meddizzle316@gmail.com>
2023-12-22 19:07:00 -0600

    hopefully this works

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
-size_t binary_tree_height_recursion(const binary_tree_t *tree);
+
+
+/**
+ * binary_tree_height_recursion - measures the height of a binary tree
+ * @tree: root node of the tree to measure
+ *
+ * Return: the height or 0 if fail
+ */
+size_t binary_tree_height_recursion(const binary_tree_t *tree)
+{
+	int lheight = 0, rheight = 0;
+
+	if (tree == NULL)
+		return (0);
+	lheight = binary_tree_height_recursion(tree->left);
+	rheight = binary_tree_height_recursion(tree->right);
+	if (lheight > rheight)
+		return (lheight + 1);
+	else
+		return (rheight + 1);
+}
 
 /**
  * binary_tree_height2 -- a height function that doesn't -1 at the end
@@ -10,7 +30,7 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return  (binary_tree_height_recursion(tree));
+	return (binary_tree_height_recursion(tree));
 }
 
 /**
